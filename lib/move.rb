c61ea39836037baee7d67c7d5b72c078ff4ffcaa
Edward Mixon <edwardchase@gatech.edu>
2018-06-08 21:04:26 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,7 +7,7 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
-def move(input)
+def move(board,index,character)
   def input_to_integer()
     position = input.to_i -1;
   end  pos = input - 1;
